44b6f8722e267a7979f2b8cc7fadda0ddf4c7c5e
designxcel <designxcel08@gmail.com>
2024-03-03 20:02:27 +0600

    added connect section

---
+++ src/Pages/Connect/Connect.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import Heading from "../../Components/Heading/Heading";
+import Paragraph from "../../Components/Paragraph/Paragraph";
+import { FaArrowRightLong } from "react-icons/fa6";
+import m3Img from "../../assets/images/m3.png"
+
+const Connect = () => {
+    return (
+        <div className="mb-20">
+            <div className="flex gap-20 items-center">
+                <div className="flex-1">
+                    <img src={m3Img} alt="" />
+                </div>
+                <div className="flex-1 space-y-8">
+                    <Heading title="Connect your tools, close your tabs"></Heading>
+                    <Paragraph description="Whether you want to edit your Google Docs, resolve Jira issues, or collaborate over Zoom, Miro has 100+ integrations with tools you already use and love."></Paragraph>
+
+                    <div className="flex gap-4 items-center text-blue-500">
+                        <Link>
+                            <h2>Learn more</h2>
+                        </Link>
+                        <FaArrowRightLong />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Connect;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../../Components/Navbar/Navbar";
 import Banner from "../Banner/Banner";
 import Collaborate from "../Collaborate/Collaborate";
+import Connect from "../Connect/Connect";
 import Together from "../Together/Together";
 import Trusted from "../Trusted/Trusted";
 
@@ -14,6 +15,7 @@ const Home = () => {
                 <Trusted></Trusted>
                 <Collaborate></Collaborate>
                 <Together></Together>
+                <Connect></Connect>
             </div>
             
         </div>
